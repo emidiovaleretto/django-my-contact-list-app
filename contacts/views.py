399b3a1faeac8f1e076af97ebd39b2061f199359
Emidio Valereto <emidio.valereto@gmail.com>
2021-06-10 16:47:47 +0100

    implements new features

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404
 from django.core.paginator import Paginator
+from django.contrib import messages
 from .models import Contact
 
 
@@ -14,6 +16,31 @@ def index(request):
 
 def details(request, contact_id):
     contacts = get_object_or_404(Contact, id=contact_id)
+
+    if not contacts.to_show:
+        raise Http404()
+
     return render(request, 'contacts/details.html', {
+        'contacts': contacts
+    })
+
+def search(request):
+    term = request.GET.get('term')
+
+    if term is None or not term:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'This field must be filled.'
+        )
+        return redirect('index')
+
+    contacts = Contact.objects.all().filter(
+        name__icontains = term,
+    )
+    paginator = Paginator(contacts, 5)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    return render(request, 'contacts/search.html', {
         'contacts': contacts
     })
